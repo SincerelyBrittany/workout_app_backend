509a4f5b1e1750d5eb43e42d1cd53cf5b3f45b3f
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-18 21:15:56 -0400

    removes render json and adds validation methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::API
-    respond_to :json
-    def render_resource(resource)
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+    rescue_from AuthorizationError, with: :unauthorized_error
+
+    def render_resource(resource, with: nil)
         if resource.errors.empty?
-          render json: resource
+          render json: resource, include: with
         else
           validation_error(resource)
         end
@@ -20,4 +22,16 @@ class ApplicationController < ActionController::API
           ]
         }, status: :bad_request
       end
+
+    def authorize_user_resource(resource)
+      raise AuthorizationError.new if resource.user != current_user
+    end
+
+    def unauthorized_error
+      render json: { message: 'You are not authorized to make that request' }, status: 401
+    end
+
+    def not_found
+      render json: { message: 'Resource not found' }, status: 404
+    end
 end
