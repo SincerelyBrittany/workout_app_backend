4880666f73140519f1610536270ab8fc238313ff
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-18 21:16:40 -0400

    updates registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,17 +1,15 @@
 class RegistrationsController < Devise::RegistrationsController
-    respond_to :json
-  
+  respond_to :json
+
     def create
-      begin 
+        begin 
             super
-      rescue ActiveRecord::RecordInvalid => e
-        render_resource(e.record)
-      rescue ActiveRecord::RecordNotUnique => e
-        err = OpenStruct.new(errors: (user: 'already Exists'))
-        validation_error(err)
-    #   build_resource(sign_up_params)
-    #   resource.save
-    #   render_resource(resource)
+        rescue ActiveRecord::RecordInvalid => e
+            render_resource(e.record)
+        rescue ActiveRecord::RecordNotUnique => e
+            err = OpenStruct.new(errors: { detail: 'User Already Exists' })
+            validation_error(err)
+        end
+
     end
-  end
 end 
